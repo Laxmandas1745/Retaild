e6d2cab050ce8de9326f4cdc69983fc825472db2
laxmandas1745 <ldas715@gmail.com>
2020-06-07 05:55:48 -0700

    Please read comments in MainActivuty, I have already made folders as a containers, we will handle them.

--- app/src/main/java/com/example/retaild/MainActivity.kt
+++ app/src/main/java/com/example/retaild/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.retaild
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+//THIS SCREEN WILL BE SPLASH SCREEN, IF THERE's NO CONNECTION, IT WILL START THE NoConnection.kt , IF ITS ONLINE and used without login, IT WILL
+//START SlideScreen, AFTER Slidesceen, it will start Login Activty., if its already logined, it will start MainScreen.
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
